70455febf44543eeea148c34563b8da4adf145e1
Luisa23121 <luisamaria23121@gmail.com>
2025-03-31 18:14:20 -0500

    Añadido cambios en funcion

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function calculadora(num1, num2, operador) {
 
-    
+    switch (operador) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num2 !== 0 ? num1 / num2 : "No se puede dividir por 0";
+        default:
+            return "Operación no válida";
+    }
 }
+
